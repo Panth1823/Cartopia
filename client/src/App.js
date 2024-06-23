c7b1add900d881f789270eb8e6a631139c5b12e8
Panth Shah <panthshah242@gmail.com>
2024-06-23 23:09:57 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./components/Home/Home";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import Category from "./components/Category/Category";
+import SingleProduct from "./components/SingleProduct/SingleProduct";
+import NewsLetter from "./components/Footer/Newsletter/Newsletter";
+import AppContext from "./utils/context";
+function App() {
+  return (
+    <BrowserRouter>
+      <AppContext>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/category/:id" element={<Category />} />
+          <Route path="/product/:id" element={<SingleProduct />} />
+        </Routes>
+        <NewsLetter />
+        <Footer />
+      </AppContext>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ client/src/components/Home/Home.jsx
@@ -0,0 +1,6 @@
+import "./Home.scss";
+const Home = () => {
+    return <div className="home">Home</div>;
+};
+
+export default Home;
