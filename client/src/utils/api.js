8c43169af25f73d3b14a1bad3c09744dd4a17f3e
Panth Shah <panthshah242@gmail.com>
2024-08-05 00:41:45 +0530

    update api.js

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -9,7 +9,7 @@ const params = {
 export const fetchDataFromApi = async (url) => {
     try {
         const { data } = await axios.get(
-            import.meta.env.VITE_REACT_APP_API_BASE_URL + url,
+            import.meta.env.VITE_REACT_APP_DEV_URL + url,
             params
         );
         return data;
@@ -20,7 +20,7 @@ export const fetchDataFromApi = async (url) => {
 };
 
 export const makePaymentRequest = axios.create({
-    baseURL: import.meta.env.VITE_REACT_APP_API_BASE_URL,
+    baseURL: import.meta.env.VITE_REACT_APP_DEV_URL,
     headers: {
         Authorization: "Bearer " + import.meta.env.VITE_REACT_APP_STRIPE_APP_KEY,
     },
