28bfd42a4f1e27f4b74022137c6cffdca8f9b464
Panth Shah <panthshah242@gmail.com>
2024-08-05 23:19:40 +0530

    added vercel speed insight

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import "./index.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
+    <SpeedInsights />
   </React.StrictMode>
 );
