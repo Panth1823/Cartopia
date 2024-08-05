74ac22168a50badd0af2f24bb42681304769100d
Panth Shah <panthshah242@gmail.com>
2024-08-05 23:24:32 +0530

    added vercel analytics

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,10 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { SpeedInsights } from "@vercel/speed-insights/react";
 import "./index.scss";
-
+import { Analytics } from "@vercel/analytics/react";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
+    <Analytics />
     <SpeedInsights />
   </React.StrictMode>
 );
